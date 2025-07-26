752cc3529b1a2b6c6e681f5084f61a7b90cf633e
KOD ﾂ <kod_.@alMacBook-Air-alkhas-bKOD.local>
2025-07-26 21:52:17 +0400

    first commit

---
+++ src/SleepIn.java
@@ -0,0 +1,11 @@
+public class SleepIn{
+    public boolean SleepIn(boolean weekday, boolean vacation) {
+        boolean sleep;
+        if (!weekday || vacation) {
+            sleep = true;
+        } else {
+            sleep = false;
+        }
+        return sleep;
+    }
+}
