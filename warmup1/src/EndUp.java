10861d846e9de622ef5c43a0a471188b6a9c0f6c
Idris Yaqoob <eya.933@live.com>
2025-07-27 04:29:38 +0400

    Delete warmup1 directory

--- warmup1/src/EndUp.java
+++
@@ -1,9 +0,0 @@
-public String endUp(String str) {
-  if (str.length() < 3) {
-    return str.toUpperCase();
-  }
-  String start = str.substring(0, str.length() - 3);
-  String end = str.substring(str.length() - 3).toUpperCase();
-  return start + end;
-}
-
--- warmup1/src/EveryNth.java
+++
@@ -1,9 +0,0 @@
-public String everyNth(String str, int n) {
-  String result = "";
-  for (int i = 0; i < str.length(); i += n) {
-    result += str.charAt(i);
-  }
-  return result;
-}
-
-
