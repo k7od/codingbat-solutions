1a1e1ac0355d512f63c934b8ec55f8242ae5faa3
Idris Yaqoob <eya.933@live.com>
2025-07-27 04:22:27 +0400

    Delete warmup1 directory

--- warmup1/src/IcyHot.java
+++
@@ -1,5 +0,0 @@
-public boolean icyHot(int temp1, int temp2) {
-  return (temp1 < 0 && temp2 > 100) || (temp2 < 0 && temp1 > 100);
-}
-
-
--- warmup1/src/LoneTeen.java
+++
@@ -1,7 +0,0 @@
-public boolean loneTeen(int a, int b) {
-  boolean Ateen = a >= 13 && a <= 19;
-  boolean Bteen = b >= 13 && b <= 19;
-  return Ateen ^ Bteen;
-}
-
-
