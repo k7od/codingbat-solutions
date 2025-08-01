8027cc36639dd4ca84a0a2521fa725a252561a9f
Idris Yaqoob <eya.933@live.com>
2025-08-01 18:48:11 +0400

    Add solution for recursion1-array220

---
+++ array220.java
@@ -0,0 +1,12 @@
+public boolean array220(int[] nums, int index) {
+  if(index>= nums.length - 1) {
+    return false;
+  }
+  else if(nums[index]* 10 == nums[index + 1]) {
+    return true;
+  }
+  else{
+    return array220(nums, index + 1);
+  }
+}
+
