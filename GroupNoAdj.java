7753f1113ef478e8ef71d05389db91057a308337
Idris Yaqoob <eya.933@live.com>
2025-08-01 21:20:27 +0400

    Add solution for recursion-groupNoAdj

---
+++ GroupNoAdj.java
@@ -0,0 +1,9 @@
+public boolean groupNoAdj(int start, int[] nums, int target) {
+  if(start>= nums.length) {
+    return target == 0;
+  }
+  else {
+    return groupNoAdj(start+2, nums,target - nums[start]) ||groupNoAdj(start+1, nums,target);
+  }
+}
+
