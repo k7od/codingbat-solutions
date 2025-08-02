7afa7bd2155bc9c0845b7d7da82a9cc772618ff6
Idris Yaqoob <eya.933@live.com>
2025-08-02 23:55:03 +0400

    Add solution for ap1-scores100

---
+++ scores100.java
@@ -0,0 +1,10 @@
+public boolean scores100(int[] scores) {
+  for (int i = 1; i < scores.length; i++) {
+    
+    if (scores[i] == 100 && scores[i-1] == 100) {
+      return true;
+    }
+  }
+  return false;
+}
+
