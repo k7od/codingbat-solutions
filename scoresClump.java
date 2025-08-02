f0ab09fa062928b5fb758e77c7d035ec3b0217d5
Idris Yaqoob <eya.933@live.com>
2025-08-03 00:21:30 +0400

    Adding solution for ap1-scoresClump problem

---
+++ scoresClump.java
@@ -0,0 +1,11 @@
+public boolean scoresClump(int[] scores) {
+  for (int i = 0; i < scores.length - 2; i++) {
+    int max = Math.max(scores[i], Math.max(scores[i+1], scores[i+2]));
+    int min = Math.min(scores[i], Math.min(scores[i+1], scores[i+2]));
+    if (max - min <= 2) {
+      return true;
+    }
+  }
+  return false;
+}
+
