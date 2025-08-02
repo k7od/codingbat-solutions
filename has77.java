750a582d67263259dd335d37a959509e4bb77acb
Idris Yaqoob <eya.933@live.com>
2025-08-02 20:40:15 +0400

    Add solution for array2-has77

---
+++ has77.java
@@ -0,0 +1,16 @@
+public boolean has77(int[] nums) {
+  for (int i=0; i < nums.length - 2; i++) {
+    if(nums[i]==7) {
+      if(nums[i+1]== 7 || nums[i+2] == 7) {
+        return true;
+      }
+    }
+  }
+  if (nums.length >= 2) {
+    return nums[nums.length - 2] == 7 && nums[nums.length - 1] == 7;
+  }
+  else{
+    return false;
+  }
+}
+
