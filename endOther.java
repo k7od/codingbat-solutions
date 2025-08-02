4582068bed918b31f02879e431464eeb76133915
Idris Yaqoob <eya.933@live.com>
2025-08-02 22:29:36 +0400

    Add solution for string2-endOther

---
+++ endOther.java
@@ -0,0 +1,12 @@
+public boolean endOther(String a, String b) {
+  a= a.toLowerCase();
+  b= b.toLowerCase();
+
+  if (a.length() >= b.length()) {
+    return a.substring(a.length() - b.length()).equals(b); }
+  
+  else {
+    return b.substring(b.length() - a.length()).equals(a);
+  }
+}
+
