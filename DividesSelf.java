77add7b17e4f4afeeebd4cdf58589e060fbd8ac1
Idris Yaqoob <eya.933@live.com>
2025-08-03 00:28:10 +0400

    Add solution for ap1-dividesSelf problem

---
+++ DividesSelf.java
@@ -0,0 +1,11 @@
+public boolean dividesSelf(int n) {
+  for (int temp = n; temp > 0; temp /= 10) {
+    
+    int digit = temp % 10;
+    if (digit == 0 || n % digit != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
